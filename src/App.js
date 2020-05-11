1555d0492850d1e4a9fc988565491bbd1837652c
Rance Campbell <rancecampbell@Rances-MBP.hitronhub.home>
2020-05-11 16:12:50 -0600

    create fake data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
+import Todos from './components/Todos';
 
 class App extends Component{
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Do the dishes',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Grab chinese food',
+        completed: false
+      }
+    ]
+  }
+
   render () {
+    console.log(this.state.todos);
     return (
       <div className="App">
         <h1>App</h1>
+        <Todos todos={this.state.todos}/>
       </div>
     );
   }
