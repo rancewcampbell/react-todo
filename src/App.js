fb28ce9df5ce89e78eeb4b294a2093281012626d
Rance Campbell <rancecampbell@Rances-MBP.hitronhub.home>
2020-05-11 18:59:51 -0600

    http delete todo

--- src/App.js
+++ src/App.js
@@ -27,17 +27,15 @@ class App extends Component{
 
   // Delete Todo
   delTodo = (id) => {
-    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] })
+    axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(res => this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] }))
   }
 
   // Add Todo
   addTodo = (title) => {
-    const newTodo = {
-      id: 4,
-      title,
-      completed: false
-    }
-    this.setState({ todos: [...this.state.todos, newTodo]})
+    axios.post('https://jsonplaceholder.typicode.com/todos', { title, completed: false })
+      .then(res => this.setState({ todos: [...this.state.todos, res.data] }))
+    
   }
 
   render () {
