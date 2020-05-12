65ec5dd735c7352820007de870f1bff1df86a6c0
Rance Campbell <rancecampbell@Rances-MBP.hitronhub.home>
2020-05-11 18:28:28 -0600

    Add todo function added

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,14 +1,28 @@
 import React, { Component } from 'react'
 
 export class AddTodo extends Component {
+  state = {
+    title: ''
+  }
+
+  onChange = (event) => this.setState({ [event.target.name]: event.target.value });
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    this.props.addTodo(this.state.title);
+    this.setState({title: ''});
+  }
+
   render() {
     return (
-      <form style={{ display: 'flex' }}>
+      <form onSubmit={this.onSubmit} style={{ display: 'flex' }}>
         <input 
           type="text"
           name="title"
           style={{ flex: '10', padding: '5px' }}
           placeholder="Add Todo ..."
+          onChange={this.onChange}
+          value={this.state.title}
         />
         <input 
           type="submit" 
